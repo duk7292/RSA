49339ce12955a604078fcd71dcbb0d9205acd03d
duk7292 <duk7292@gmail.com>
2025-11-19 00:19:31 +0100

    m

--- RSA.java
+++ RSA.java
@@ -1,14 +1,46 @@
 import java.math.BigInteger;
+import java.util.Random;
 
 public class RSA {
     private BigInteger d, e, n;
 
-    BigInteger eulerPhi(BigInteger p, BigInteger q) {
+    BigInteger eulerPhi(BigInteger p, BigInteger q) 
+    {
         BigInteger result = (p.subtract(BigInteger.ONE)).multiply((q.subtract(BigInteger.ONE)));
         return result;
     }
 
-    static Boolean lessThan(BigInteger a, BigInteger b) {
+    static Boolean lessThan(BigInteger a, BigInteger b) 
+    {
         return a.compareTo(b) < 0;
+    }
+    private Boolean isValidNumber(BigInteger e, BigInteger x)
+    {   
+        if(!e.gcd(x).equals(BigInteger.ONE) )
+        {
+            return false;
+        }
+        if(e.equals(BigInteger.ONE))
+        {
+            return false;
+        }
+        if(e.max(x).equals(e))
+        {
+            return false;
+        }
+
+        return true;
+    }
+    BigInteger generateCoprime(BigInteger x)
+    {
+        BigInteger a;
+
+
+        do{
+            a = (new BigInteger(x.bitLength(), new  Random()));
+
+        }while(!isValidNumber(a,x));
+
+        return a;
     }
 }
--- main.java
+++ main.java
@@ -1,7 +1,11 @@
 import java.math.BigInteger;
-
+import java.util.Random;
 void main() {
 
     RSA a = new RSA();
-    IO.println(a.lessThan(BigInteger.valueOf(4), BigInteger.valueOf(5)));
+    for(int i = 0; i < 100; i++)
+    {
+        IO.println(a.generateCoprime(a.eulerPhi(BigInteger.valueOf(53503),BigInteger.valueOf(91957))));
+    }
+
 }
